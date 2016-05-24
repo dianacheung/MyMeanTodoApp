2f523d9536a8db8b009d8c4f52a7a90d26fa8f49
dianacheung <dianacheung@users.noreply.github.com>
2016-05-23 18:58:37 -0700

    fix mongodb url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = {
-        url: 'mongodb://admin:admin@ec2-52-38-211-75.us-west-2.compute.amazonaws.com:27017/test'
+module.exports = {	
+        url: 'mongodb://dummy:dummy@ec2-52-38-211-75.us-west-2.compute.amazonaws.com:27017/dummyDB'
 		//url : 'mongodb://localhost:27017/test'
 };
