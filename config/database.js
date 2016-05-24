5fb81358eaf0c5d8b7b670489e39301d17cc9bef
dianacheung <dianacheung@users.noreply.github.com>
2016-05-23 18:24:15 -0700

    modify mongodb url

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-        url : 'mongodb://localhost:27017/test'
+        url: 'mongodb://admin:admin@ec2-52-38-211-75.us-west-2.compute.amazonaws.com:27017/test'
+		//url : 'mongodb://localhost:27017/test'
 };
